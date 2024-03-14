406a4accf83e16724ab23ed881c849a80fdcb5d3
Alexander Ereshkin <xcode111@mail.ru>
2024-03-14 16:02:39 +0300

    Make naming more concise, make lower and upper bound selectable.

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,80 @@
-extern crate core;
+use std::io::{Error as IOError, stdin, stdout, Write};
+use std::num::ParseFloatError;
+use std::process::exit;
+use std::str::FromStr;
 
 const MAX_ITERATIONS_COUNT: i64 = 1000000000;
+const DEFAULT_EPSILON: f64 = 0.0001;
+const EXIT_INCORRECT_LOWER_BOUND: i32 = 1;
+const EXIT_INCORRECT_UPPER_BOUND: i32 = 2;
 
-fn find_roots(
-    f: fn(f64) -> f64,
-    df: fn(f64) -> f64,
-    df2: fn(f64) -> f64,
+fn parse_to_double(str: &str) -> Result<f64, ParseFloatError> {
+    Ok(f64::from_str(str)?)
+}
+
+pub fn get_line<'a>() -> Result<&'a str, IOError> {
+    let mut result: String = String::new();
+    stdin().read_line(&mut result)?;
+    result = String::from(result.trim());
+    let result: &'a str = result.leak::<'a>();
+    Ok(result)
+}
+
+// FIXME function calculates not all roots. 
+// Make better algorithm than simply iterating over a domain in which we are finding roots.
+fn find_roots<'a>(
+    function: fn(f64) -> f64,
+    derivative: fn(f64) -> f64,
+    second_derivative: fn(f64) -> f64,
     epsilon: f64,
     lower_bound: f64,
     upper_bound: f64,
-) -> Vec<f64> {
+) -> &'a [f64] {
     let mut list: Vec<f64> = Vec::new();
-    let mut i = lower_bound;
-    let step = epsilon * 10.0;
-    while i + step <= upper_bound {
-        match find_root_newton(f, df, df2, i, epsilon, MAX_ITERATIONS_COUNT) {
+    let mut current_iteration_lower_bound = lower_bound;
+    let iteration_step = epsilon * 10.0;
+    while current_iteration_lower_bound + iteration_step <= upper_bound {
+        match find_root_newton(
+            function, derivative, second_derivative,
+            current_iteration_lower_bound, epsilon, MAX_ITERATIONS_COUNT,
+        ) {
             None => {
-                i += step;
+                current_iteration_lower_bound += iteration_step;
                 continue;
             }
             Some(root) => {
                 if root < lower_bound || root > upper_bound {
-                    i += step;
+                    current_iteration_lower_bound += iteration_step;
                     continue;
                 }
                 if list.iter().any(|x| { (root - x).abs() < epsilon }) {
-                    i += step;
+                    current_iteration_lower_bound += iteration_step;
                     continue;
                 }
                 list.push(root);
-                i += step;
+                current_iteration_lower_bound += iteration_step;
             }
         };
     }
-    list
+    list.leak::<'a>()
 }
 
 fn find_root_newton(
-    f: fn(f64) -> f64,
-    df: fn(f64) -> f64,
-    df2: fn(f64) -> f64,
+    function: fn(f64) -> f64,
+    derivative: fn(f64) -> f64,
+    second_derivative: fn(f64) -> f64,
     x0: f64,
     epsilon: f64,
     max_iterations: i64,
 ) -> Option<f64> {
-    if f(x0) * df2(x0) <= 0f64 {
+    if function(x0) * second_derivative(x0) <= 0f64 {
         return None;
     }
     let mut x = x0;
     let mut i = 0i64;
     while i < max_iterations {
-        let fx = f(x);
-        let dfx = df(x);
+        let fx = function(x);
+        let dfx = derivative(x);
         if fx.abs() < epsilon {
             return Some(x);
         }
@@ -64,20 +87,44 @@ fn find_root_newton(
     Some(x)
 }
 
+// TODO make user enter function in standard input or a file
+fn function(x: f64) -> f64 {
+    x * x * x * x - 4.3 * x * x * x - 1.29 * x * x + 15.1 * x.sin() + 9.84
+}
+
+// TODO calculate first and second derivative numerically rather than explicitly specifying in code.
+fn derivative(x: f64) -> f64 {
+    (151.0 / 10.0) * x.cos() + 4.0 * x * x * x - 12.9 * x * x - (129.0 / 50.0) * x
+}
+
+fn second_derivative(x: f64) -> f64 {
+    (1.0 / 50.0) * (-755.0 * x.sin() + 600.0 * x * x - 1290.0 * x - 129.0)
+}
+
 fn main() {
+    print!("Введите нижнюю границу: ");
+    stdout().flush().unwrap();
+    let lower_bound = parse_to_double(get_line().unwrap())
+        .inspect_err(|_| {
+            eprintln!("Ошибка преобразования ввода в вещественное число");
+            exit(EXIT_INCORRECT_LOWER_BOUND);
+        })
+        .unwrap();
+    print!("Введите верхнюю границу: ");
+    stdout().flush().unwrap();
+    let upper_bound = parse_to_double(get_line().unwrap())
+        .inspect_err(|_| {
+            eprintln!("Ошибка преобразования ввода в вещественное число");
+            exit(EXIT_INCORRECT_UPPER_BOUND);
+        })
+        .unwrap();
     let roots =
         find_roots(
-            |x| {
-                x * x * x * x - 4.3 * x * x * x - 1.29 * x * x + 15.1 * x.sin() + 9.84
-            },
-            |x| {
-                (151.0 / 10.0) * x.cos() + 4.0 * x * x * x - 12.9 * x * x - (129.0 / 50.0) * x
-            },
-            |x| {
-                (1.0 / 50.0) * (-755.0 * x.sin() + 600.0 * x * x - 1290.0 * x - 129.0)
-            },
-            0.0001,
-            0.0,
-            4.0);
+            function,
+            derivative,
+            second_derivative,
+            DEFAULT_EPSILON,
+            lower_bound,
+            upper_bound);
     println!("{:?}", roots)
 }
